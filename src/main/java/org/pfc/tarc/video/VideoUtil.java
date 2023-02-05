867cde40baf537bb6fe601596a24dc7228fafb67
Daniel Avila <masterav10@gmail.com>
2023-02-05 12:42:17 -0500

    Utility class for testing stuff.

--- src/main/java/org/pfc/tarc/video/VideoUtil.java
+++ src/main/java/org/pfc/tarc/video/VideoUtil.java
@@ -1,12 +1,18 @@
 package org.pfc.tarc.video;
 
+import static org.bytedeco.ffmpeg.global.avutil.*;
+
 import java.io.IOException;
 import java.util.Arrays;
 
 import org.bytedeco.javacpp.Loader;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class VideoUtil
 {
+    private static final Logger LOG = LoggerFactory.getLogger(VideoUtil.class);
+
     public static Process ffmpeg(String command, Object... args)
     {
         final String ffmpeg = Loader.load(org.bytedeco.ffmpeg.ffmpeg.class);
@@ -15,6 +21,8 @@ public class VideoUtil
 
         String[] commandArray = new String[inputArgs.length + 1];
         Arrays.setAll(commandArray, i -> i == 0 ? ffmpeg : inputArgs[i - 1]);
+
+        LOG.info("ffmpeg {}", resolved);
 
         ProcessBuilder pb = new ProcessBuilder(commandArray);
 
@@ -27,5 +35,20 @@ public class VideoUtil
         {
             throw new IllegalStateException(e);
         }
+    }
+
+    public static final int check(int error)
+    {
+        if (error != 0)
+        {
+            byte[] ptr = new byte[1024];
+            av_make_error_string(ptr, ptr.length, error);
+            String msg = String.format("%d: %s", error, new String(ptr).trim());
+
+            throw new IllegalStateException(msg);
+
+        }
+
+        return error;
     }
 }
